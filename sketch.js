8d4c4f895fdeea2b614e7b3afdf2d457e4f39eb5
sandeep200-whitehat <74446660+sandeep200-whitehat@users.noreply.github.com>
2021-05-26 21:26:44 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var  gameState, contestantCount, database, quiz, question, contestant, allContestants;
+var gameState=0;
+function setup(){
+  canvas = createCanvas(850,400);
+  database=firebase.database();
+  quiz = new Quiz();
+  quiz.getState();
+  quiz.start();
+  
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 4){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+}
